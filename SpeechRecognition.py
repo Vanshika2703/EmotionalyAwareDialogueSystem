be1c99bbdf7977b670c3da2b40a44fd761889c4a
Vanshika2703 <reddyvs@rose-hulman.edu>
2021-05-09 22:56:04 -0400

    updated speech recognition and most of the lost data

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -1,10 +1,46 @@
+#%pylab inline
+import numpy as np
 import speech_recognition as sr
-import extraction as key
+# import extraction as key
 import sys
 import pyttsx3
+import pyaudio
+import wave
+import os
+import pandas as pd
+import librosa
+import glob 
+import keras
+from keras.models import model_from_json
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
 
+CHUNK = 1024 
+FORMAT = pyaudio.paInt16 #paInt8
+CHANNELS = 2 
+RATE = 44100 #sample rate
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "output10.wav"
+lb = LabelEncoder()
+opt = keras.optimizers.RMSprop(lr=0.00001, decay=1e-6)
 
+p = pyaudio.PyAudio()
 r = sr.Recognizer()
+
+# loading json and creating model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("saved_models/Emotion_Voice_Detection_Model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+#score = loaded_model.evaluate(x_testcnn, y_test, verbose=0)
+#print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
 
 # need to find a way that can recognize speech faster 
 
@@ -21,6 +57,47 @@ def startListening():
             except:
                 print('Sorry couldn\'t recognize your voice', sys.exc_info()[0])
 
+# Recognizing emotions using the model
+def recognize():
+    data, sampling_rate = librosa.load('output10.wav')
+    X, sample_rate = librosa.load('output10.wav', res_type='kaiser_fast',duration=2.5,sr=22050*2,offset=0.5)
+    sample_rate = np.array(sample_rate)
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13),axis=0)
+    featurelive = mfccs
+    livedf2 = featurelive
+    livedf2= pd.DataFrame(data=livedf2)
+    livedf2 = livedf2.stack().to_frame().T
+    twodim= np.expand_dims(livedf2, axis=2)
+    livepreds = loaded_model.predict(twodim,batch_size=32,verbose=1)
+    livepreds1=livepreds.argmax(axis=1)
+    liveabc = livepreds1.astype(int).flatten()
+    livepredictions = (lb.inverse_transform((liveabc)))
+    return livepredictions
+
+# Recording the input for voice features
+
+def record():
+    stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK) #buffer
+    print("* recording")
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data) # 2 bytes(16 bits) per channel
+    print("* done recording")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    recognize()
 
 # the second part of the implementation in this file would be text to speech so as to give responses
 
@@ -29,8 +106,9 @@ def main():
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
-    engine.say("Hello Dr.Wollowski! My name is Ahaana. I am a little nervous because this is my first demo and I can only update the mental model so far. wish me luck! Thank you!")
+    engine.say("Hello I am here!")
     engine.runAndWait()
-    startListening()
+    #startListening()
+    record()
 
 main()
